95750cbb7cc79caab74777044d155db8d3992767
岑永洪 <cenyongh@gmail.com>
2018-02-08 23:50:41 +0800

    report

--- src/components/navigator.vue
+++ src/components/navigator.vue
@@ -2,11 +2,8 @@
   <div>
     <el-menu router mode="horizontal">
       <el-menu-item index="/">数学</el-menu-item>
-      <el-submenu index="2">
-        <template slot="title">拼音</template>
-        <el-menu-item index="/pinyin">区分声母，韵母</el-menu-item>
-        <el-menu-item index="/pinyin2">写拼音</el-menu-item>
-      </el-submenu>
+      <el-menu-item index="/pinyin">声母/韵母</el-menu-item>
+      <el-menu-item index="/pinyin2">写拼音</el-menu-item>
       <el-menu-item index="/chart">图表</el-menu-item>
       <el-menu-item index="/report">报告</el-menu-item>
     </el-menu>
