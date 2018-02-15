bc936e87c54a82133b7d733f651e3fd98e8a78fb
岑永洪 <cenyongh@gmail.com>
2018-02-15 13:21:30 +0700

    ~

--- src/packages/statics/index.vue
+++ src/packages/statics/index.vue
@@ -1,6 +1,12 @@
 <template>
   <div>
-    <div id="chart" style="width:640px;height:480px"/>
+    <setting :setting="setting" @submit="generate"/>
+    <el-card>
+      <div slot="header" class="clearfix">
+        <span>图表</span>
+      </div>
+      <div id="chart" style="width:640px;height:480px"/>
+    </el-card>
   </div>
 </template>
 
@@ -10,81 +16,90 @@ require('echarts/lib/chart/bar')
 require('echarts/lib/component/polar')
 
 import web from '@/web'
+import setting from './setting'
 
 var moment = require('moment')
 
 export default {
   data: function () {
     return {
-
+      setting: {
+        math1: 4,
+        pinyin1: 1,
+        pinyin2: 3,
+        pinyin3: 1
+      }
     }
   },
   mounted () {
-    var option = {
-      title: {
-        text: '积分'
-      },
-      xAxis: {
-        type: 'category',
-        data: []
-      },
-      yAxis: {
-        type: 'value'
-      },
-      series: [{
-        data: [],
-        type: 'bar',
-        label: {
-          normal: {
-            show: true,
-            position: 'top'
-          }
-        },
-        smooth: true
-      }]
-    }
-
-    let chart = this.$echarts.init(document.getElementById('chart'), 'light')
-
-    var tomorrow = moment().add(1, 'days')
-    var one_month_ago = moment().subtract(14, 'days')
-
-    var params = {
-      from_date: one_month_ago.format("YYYY-MM-DD"), 
-      to_date: tomorrow.format("YYYY-MM-DD"), 
-      is_correct: 1
-    }
-
-    web.list(params, ( response ) => {
-      var dates = this.getDateArray(one_month_ago, tomorrow)
-
-      var datas = {}
-
-      response.data.data.forEach( (value) => {
-        if (datas[value.date]) {
-          datas[value.date] += this.score(value)
-        } else {
-          datas[value.date] = this.score(value)
-        }
-      })
-
-      
-      dates.forEach( (date) => {
-        var val = datas[date]
-
-        if (val) {
-          option.series[0].data.push(val)
-        } else {
-          option.series[0].data.push(0)
-        }
-      })
-
-      option.xAxis.data = dates
-      chart.setOption(option)
-      
-    })
+    this.generate()
   },
   methods: {
+    generate () {
+      var option = {
+        title: {
+          text: '积分'
+        },
+        xAxis: {
+          type: 'category',
+          data: []
+        },
+        yAxis: {
+          type: 'value'
+        },
+        series: [{
+          data: [],
+          type: 'bar',
+          label: {
+            normal: {
+              show: true,
+              position: 'top'
+            }
+          },
+          smooth: true
+        }]
+      }
+
+      let chart = this.$echarts.init(document.getElementById('chart'), 'light')
+
+      var tomorrow = moment().add(1, 'days')
+      var one_month_ago = moment().subtract(14, 'days')
+
+      var params = {
+        from_date: one_month_ago.format("YYYY-MM-DD"), 
+        to_date: tomorrow.format("YYYY-MM-DD"), 
+        is_correct: 1
+      }
+
+      web.list(params, ( response ) => {
+        var dates = this.getDateArray(one_month_ago, tomorrow)
+
+        var datas = {}
+
+        response.data.data.forEach( (value) => {
+          if (datas[value.date]) {
+            datas[value.date] += this.score(value)
+          } else {
+            datas[value.date] = this.score(value)
+          }
+        })
+
+
+        dates.forEach( (date) => {
+          var val = datas[date]
+
+          if (val) {
+            option.series[0].data.push(val)
+          } else {
+            option.series[0].data.push(0)
+          }
+        })
+
+        option.xAxis.data = dates
+        chart.setOption(option)
+
+      })
+    },
     getDateArray (start, end) {
       var arr = [];
 
@@ -100,21 +115,24 @@ export default {
       switch (value.kind) 
       {
         case 0:
-        weight = 4
+        weight = this.setting.math1
         break;
         case 1:
-        weight = 1
+        weight = this.setting.pinyin1
         break;
         case 2:
-        weight = 3
+        weight = this.setting.pinyin2
         break;
         case 3:
-        weight = 1
+        weight = this.setting.pinyin3
         break;
         default:
       }
       return weight
     }
+  },
+  components: {
+    setting
   }
 }
 </script>
---
+++ src/packages/statics/setting.vue
@@ -0,0 +1,48 @@
+<template>
+  <div>
+    <el-card>
+      <div slot="header" class="clearfix">
+        <span>难度</span>
+      </div>
+      <el-form :model="setting" label-width="100px">
+        <el-form-item label="数学">
+          <el-slider v-model="setting.math1" show-stops :min="0" :max="5">
+          </el-slider>
+        </el-form-item>
+        <el-form-item label="声母/韵母">
+          <el-slider v-model="setting.pinyin1" show-stops :min="0" :max="5">
+          </el-slider>
+        </el-form-item>
+        <el-form-item label="写拼音">
+          <el-slider v-model="setting.pinyin2" show-stops :min="0" :max="5">
+          </el-slider>
+        </el-form-item>
+        <el-form-item label="选生字">
+          <el-slider v-model="setting.pinyin3" show-stops :min="0" :max="5">
+          </el-slider>
+        </el-form-item>
+        <el-form-item>
+          <el-button type="primary" @click="submit">计算</el-button>
+        </el-form-item>
+      </el-form>
+    </el-card>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    setting: {
+      type: Object
+    }
+  },
+  methods: {
+    submit () {
+      this.$emit('submit')
+    },
+    restart () {
+      this.$emit('restart')
+    }
+  }
+}
+</script>
